4061e59e7f4153127b273488318c016adc3837de
Kevin Carrillo <kcarrillo@joonik.com>
2023-10-24 21:02:20 -0500

    add images and text to herocard

--- src/heroes/pages/DcPage.jsx
+++ src/heroes/pages/DcPage.jsx
@@ -1,3 +1,13 @@
+import { HeroList } from "../components";
+
 export const DcPage = () => {
-  return <h1>DCPage</h1>;
+  return (
+    <>
+      <h1>DC Comics</h1>
+      <hr />
+      <ul>
+        <HeroList publisher="DC Comics" />
+      </ul>
+    </>
+  );
 };
